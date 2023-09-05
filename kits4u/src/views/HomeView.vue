4d78f8b3bae82c1751534fc23e138c37f0bd8d9f
Uthmaan <samsodienuthmaan@gmail.com>
2023-09-05 12:12:10 +0200

    updates

--- kits4u/src/views/HomeView.vue
+++ kits4u/src/views/HomeView.vue
@@ -3,16 +3,19 @@
     <div class="container text-center">
       <div class="row align-items-center">
         <div class="col">
-          <img
-            src="https://i.postimg.cc/DZhtqh3W/1-removebg-preview-2.png"
+          <img class="homeImg"
+            src="https://i.postimg.cc/90xgz3fS/result.png"
             alt="logo"
           />
         </div>
-        <div class="col name">WELCOME TO KITS 4 U</div>
+        <div class="col name">WELCOME TO KITS 4  U</div>
         <div><button class="btn btn-home" href="./ProductsView.vue">See products:</button>
           <button class="btn btn-home" href="./ContactView.vue">Contact us:</button>
         </div>
       </div>
+      <!-- <div class="bgImg">
+        <img src="https://i.postimg.cc/2ySp67CH/CELEBS-IN-FOOTBALL-SHIRTS.webp" alt="logo">
+      </div> -->
     </div>
     <!-- <div class="homepic">
       <img src="https://i.postimg.cc/SKg2Yb5L/sports-cup-crowd-fans-silhouette-vector-35552718-removebg-preview.png" id="homepic" alt="logo">
@@ -38,9 +41,15 @@ export default {
 :hover.btn-home{
   color: #d72323 !important ;
 }
-.homepic{
-  width: 100%;
-}
 
+.bgImg{
+  background-image: url(https://i.postimg.cc/2ySp67CH/CELEBS-IN-FOOTBALL-SHIRTS.webp);
+  background-attachment: scroll;
+  background-size:  contain;
+  opacity: 90%;
+}
+.homeImg{
+  width: 30rem;
+}
 
 </style> 
